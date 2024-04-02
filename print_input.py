62475b274311e29a6ef7ed2497b263812ee001d3
Espazam <wesleymmenezes@hotmail.com>
2024-04-02 10:50:40 -0300

    Aula_print_input

---
+++ print_input.py
@@ -0,0 +1,7 @@
+nome = input("Informe o seu nome: ")
+sobrenome = input("Informe seu sobrenome: ")
+idade = input("Informe a sua idade: ")
+
+print (nome, sobrenome, idade, end="...\n")
+print (nome, sobrenome, idade, sep="#", end="...\n")
+print (nome, sobrenome, idade, sep="#")
