c83b216ee4779f53e79d784cb859ec01f2bdec25
Espazam <wesleymmenezes@hotmail.com>
2024-04-02 09:52:18 -0300

    Aula 3

--- Primeiro programa.py
+++ Primeiro programa.py
@@ -1,6 +1,15 @@
 print("Hello world!")
+print("Seja bem vindo ao curso de Python")
 print(11 + 10)
 print(1.5 + 1 + 0.5)
 print(True)
 print(False)
 print("Python")
+
+age = 29
+name = 'Wesley'
+print( f'Meu nome é {name} e tenho {age} ano(s) de idade.')
+
+age = 32
+name = 'Guilherme'
+print(f'Meu nome é {name} e tenho {age} ano(s) de idade.')
