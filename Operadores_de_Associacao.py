5a2f413f070ca3c7aaa8f717dbcdf6fe250b4ec0
Espazam <wesleymmenezes@hotmail.com>
2024-04-03 10:15:10 -0300

    operadores de associação

---
+++ Operadores_de_Associacao.py
@@ -0,0 +1,12 @@
+# Utilizados para verificar se um objeto está presente em uma sequência
+# IN se está na sequência
+# NOT IN se não está na sequencia
+
+frutas = ["limão", "uva"]
+frutas = ["limão", "uva", "laranja"] 
+curso = "Curso de Python"
+
+print("laranja" in frutas)
+print("laranja" in frutas)
+print("limão" not in frutas)
+print("Python" in curso)
