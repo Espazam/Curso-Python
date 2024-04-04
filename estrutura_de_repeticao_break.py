e6039e84d9115254e63db70bba52e841fda82b8a
Espazam <wesleymmenezes@hotmail.com>
2024-04-04 10:52:58 -0300

    Estrutura de Repetição 2

--- estrutura_de_repeticao_break.py
+++ estrutura_de_repeticao_break.py
@@ -1,5 +1,13 @@
-while True:
-    numero = int(input("Informe um número: "))
+# while True:
+#    numero = int(input("Informe um número: "))
+
+#    if numero == 10:
+#        break
+
+#    print(numero)
+
+
+for numero in range(100):
 
     if numero == 10:
         break
